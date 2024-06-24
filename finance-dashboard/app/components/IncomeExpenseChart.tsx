b63c99f1592fa9a907e3b50b86a8a9a9891076bc
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-06-24 15:07:53 -0700

    feat: added chart for income and expenses on the dashboard

---
+++ finance-dashboard/app/components/IncomeExpenseChart.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Chart from "../components/Chart";
+
+const IncomeExpenseChart = () => {
+	return (
+		<div className="w-[37rem] h-[280px] bg-[#000000] text-white text-lg p-[10px] rounded-md mt-5 ml-10">
+			<Chart />
+		</div>
+	);
+};
+
+export default IncomeExpenseChart;
