bdacafcb3c71178be72309c11206525db5f332df
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-06-25 15:29:26 -0700

    updated paths for the nav links

--- finance-dashboard/data.tsx
+++ finance-dashboard/data.tsx
@@ -11,26 +11,31 @@ const navLinks = [
 		name: "dashboard",
 		label: "Dashboard",
 		icon: <MdSpaceDashboard className="text-2xl ml-3" />,
+		path: "/dashboard",
 	},
 	{
 		name: "calender",
 		label: "Calender",
 		icon: <SlCalender className="text-2xl ml-3" />,
+		path: "/calender",
 	},
 	{
 		name: "settings",
 		label: "Settings",
 		icon: <IoSettingsSharp className="text-2xl ml-3" />,
+		path: "/calender",
 	},
 	{
 		name: "favorite",
 		label: "Favorite",
 		icon: <MdOutlineFavorite className="text-2xl ml-3" />,
+		path: "/calender",
 	},
 	{
 		name: "account",
 		label: "Account",
 		icon: <MdAccountCircle className="text-2xl ml-3" />,
+		path: "/calender",
 	},
 ];
 
