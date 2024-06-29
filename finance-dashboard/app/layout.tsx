b4cda6ab6ee0cf7dec88ef386178237a75dc6f7d
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-06-29 16:27:30 +0100

    fix: made some changes to layout.tsx

--- finance-dashboard/app/layout.tsx
+++ finance-dashboard/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Personal Finance Dashboard",
+	title: "Finance Dashboard",
 };
 
 export default function RootLayout({
